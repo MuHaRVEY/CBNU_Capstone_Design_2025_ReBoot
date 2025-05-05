0c0aac00a76a2a3d0d01332058dd32ecbdbd96f0
AhnJewon <inno6734@gmail.com>
2025-05-05 11:28:14 +0900

    tmap API 연동동

--- capstonedesign/lib/gmap_test.dart
+++ capstonedesign/lib/gmap_test.dart
@@ -1,6 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
+import 'package:location/location.dart' as LAC;
 import 'package:geolocator/geolocator.dart';
 
 class MapStateTest extends StatefulWidget{
@@ -18,6 +20,16 @@ class _MapStateTest extends State<MapStateTest>{
     mapController = controller;
   }
 
+  final List<Marker> markers = [];
+
+  _addMarker(cordinate) {
+    int id = Random().nextInt(100);
+
+    setState(() {
+      markers.add(Marker(position: cordinate, markerId: MarkerId(id.toString())));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,15 +38,51 @@ class _MapStateTest extends State<MapStateTest>{
           title: const Text('Maps Sample App'),
           backgroundColor: Colors.green[700],
         ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          myLocationEnabled: true,
-          myLocationButtonEnabled: true,
-          initialCameraPosition: CameraPosition(
-              target: _center,
-          zoom: 20.0
-          ),
-        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height*0.7,
+              width: MediaQuery.of(context).size.width,
+              child: GoogleMap(
+                onMapCreated: _onMapCreated,
+                myLocationEnabled: true,
+                myLocationButtonEnabled: true,
+                initialCameraPosition: CameraPosition(
+                    target: _center,
+                    zoom: 11.0
+                ),
+                markers: markers.toSet(),
+                onTap: (cordinate) {
+                  mapController.animateCamera(CameraUpdate.newLatLng(cordinate));
+                  _addMarker(cordinate);
+                },
+              ),
+              ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                FloatingActionButton(
+              onPressed: () async {
+                var gps = await getCurrentLocation();
+
+                mapController.animateCamera(
+                    CameraUpdate.newLatLng(LatLng(gps.latitude, gps.longitude)));
+
+              },
+              backgroundColor: Colors.white,
+              child: Icon(
+                Icons.my_location,
+                color: Colors.black,
+              ),
+            ),
+              ],
+              ),
+          ],
+        )
+
       )
     );
   }
@@ -42,22 +90,8 @@ class _MapStateTest extends State<MapStateTest>{
 
 Future<Position> getCurrentLocation() async {
   Position position = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high);
+      locationSettings: LocationSettings(accuracy: LocationAccuracy.high));
 
   return position;
 }
 
-FloatingActionButton(
-onPressed: () async {
-var gps = await getCurrentLocation();
-
-_controller.animateCamera(
-CameraUpdate.newLatLng(LatLng(gps.latitude, gps.longitude)));
-
-},
-child: Icon(
-Icons.my_location,
-color: Colors.black,
-),
-backgroundColor: Colors.white,
-),
