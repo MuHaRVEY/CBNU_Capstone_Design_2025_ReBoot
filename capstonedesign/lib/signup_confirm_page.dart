a0fc28ea97ac10e5db213f46ed725b18b480fb8a
Bak Hyeongwoo <toplionby421@gmail.com>
2025-05-06 21:22:19 +0900

    Revert "feat : DB에 등록되게 설정"
    This reverts commit 1017f4dc586b054416ab8ae64fb25ca59b769a77.

--- capstonedesign/lib/signup_confirm_page.dart
+++ capstonedesign/lib/signup_confirm_page.dart
@@ -1,43 +1,28 @@
 import 'package:flutter/material.dart';
-import 'homepage.dart'; // 홈화면 파일 import
 
-class SignupConfirmPage extends StatefulWidget {
+class SignupConfirmPage extends StatelessWidget {
   const SignupConfirmPage({Key? key}) : super(key: key);
-
-  @override
-  _SignupConfirmPageState createState() => _SignupConfirmPageState();
-}
-
-class _SignupConfirmPageState extends State<SignupConfirmPage> {
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => HomePage()), // 홈화면으로 이동
-      );
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
+          // 배경 이미지
           Positioned.fill(
             child: Image.asset(
-              'assets/images/image_firstpage_login.png',
+              'assets/images/image_firstpage_login.png', // 이미지 경로 확인 필요
               fit: BoxFit.cover,
             ),
           ),
+          // 가운데 텍스트
           Center(
             child: Text(
               '회원가입이 완료되었습니다.',
               style: TextStyle(
                 fontSize: 18.0,
                 fontWeight: FontWeight.w500,
-                color: Colors.black,
+                color: Colors.black, // 배경에 따라 필요시 색 조정
               ),
               textAlign: TextAlign.center,
             ),
