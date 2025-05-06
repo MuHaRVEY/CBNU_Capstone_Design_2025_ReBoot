7118848b4179cf6b5d8ae360402257adb508730f
Bak Hyeongwoo <toplionby421@gmail.com>
2025-05-06 21:20:51 +0900

    feat : homepage로 이동하게 변경

--- capstonedesign/lib/signup_confirm_page.dart
+++ capstonedesign/lib/signup_confirm_page.dart
@@ -1,28 +1,43 @@
 import 'package:flutter/material.dart';
+import 'homepage.dart'; // 홈화면 파일 import
 
-class SignupConfirmPage extends StatelessWidget {
+class SignupConfirmPage extends StatefulWidget {
   const SignupConfirmPage({Key? key}) : super(key: key);
+
+  @override
+  _SignupConfirmPageState createState() => _SignupConfirmPageState();
+}
+
+class _SignupConfirmPageState extends State<SignupConfirmPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()), // 홈화면으로 이동
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          // 배경 이미지
           Positioned.fill(
             child: Image.asset(
-              'assets/images/image_firstpage_login.png', // 이미지 경로 확인 필요
+              'assets/images/image_firstpage_login.png',
               fit: BoxFit.cover,
             ),
           ),
-          // 가운데 텍스트
           Center(
             child: Text(
               '회원가입이 완료되었습니다.',
               style: TextStyle(
                 fontSize: 18.0,
                 fontWeight: FontWeight.w500,
-                color: Colors.black, // 배경에 따라 필요시 색 조정
+                color: Colors.black,
               ),
               textAlign: TextAlign.center,
             ),
