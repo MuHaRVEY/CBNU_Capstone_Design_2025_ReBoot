d327f7b54f3dd847ec575427f7aac175ff621915
AhnJewon <inno6734@gmail.com>
2025-05-31 20:44:16 +0900

    통합 완료

--- capstonedesign/android/app/build.gradle.kts
+++ capstonedesign/android/app/build.gradle.kts
@@ -10,9 +10,9 @@ plugins {
 
 android {
     namespace = "com.dev.capstone.capstonedesign"
-    compileSdk = flutter.compileSdkVersion
-//    ndkVersion = flutter.ndkVersion
-    ndkVersion = "27.0.12077973" // ✅ 이 줄을 추가하세요
+    compileSdk = 35
+    ndkVersion = flutter.ndkVersion
+    //ndkVersion = "27.0.12077973" // ✅ 이 줄을 추가하세요
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -27,7 +27,7 @@ android {
         applicationId = "com.dev.capstone.capstonedesign"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
