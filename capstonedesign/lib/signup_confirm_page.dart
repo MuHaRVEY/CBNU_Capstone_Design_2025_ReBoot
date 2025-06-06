f1e48ea336bc7e0dd2e201e336bf195a9203ccd1
Bak Hyeongwoo <toplionby421@gmail.com>
2025-06-06 15:29:17 +0900

    feat : 회원가입시 homepage로 자동으로 넘어가는 기능 추가

--- capstonedesign/lib/signup_confirm_page.dart
+++ capstonedesign/lib/signup_confirm_page.dart
@@ -1,28 +1,58 @@
 import 'package:flutter/material.dart';
+import 'homepage.dart';
 
-class SignupConfirmPage extends StatelessWidget {
-  const SignupConfirmPage({Key? key}) : super(key: key);
+class SignupConfirmPage extends StatefulWidget {
+  final String userId;
+  final String nickname;
+
+  const SignupConfirmPage({
+    Key? key,
+    required this.userId,
+    required this.nickname,
+  }) : super(key: key);
+
+  @override
+  State<SignupConfirmPage> createState() => _SignupConfirmPageState();
+}
+
+class _SignupConfirmPageState extends State<SignupConfirmPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 4), () {
+      if (mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomePage(
+              userId: widget.userId,
+              userName: widget.nickname,
+            ),
+          ),
+        );
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          // 배경 이미지
           Positioned.fill(
             child: Image.asset(
-              'assets/images/image_firstpage_login.png', // 이미지 경로 확인 필요
+              'assets/images/image_firstpage_login.png',
               fit: BoxFit.cover,
             ),
           ),
-          // 가운데 텍스트
-          Center(
+          const Center(
             child: Text(
               '회원가입이 완료되었습니다.',
               style: TextStyle(
                 fontSize: 18.0,
                 fontWeight: FontWeight.w500,
-                color: Colors.black, // 배경에 따라 필요시 색 조정
+                color: Colors.black,
               ),
               textAlign: TextAlign.center,
             ),
