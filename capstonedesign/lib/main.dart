32dd1f3e8a7150c50a9b1d9d16e082d350dae664
Bak Hyeongwoo <toplionby421@gmail.com>
2025-09-18 20:27:13 +0900

    build : 빌드 파일 수정

--- capstonedesign/lib/main.dart
+++ capstonedesign/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'firebase/firebase_options.dart';
 import 'package:provider/provider.dart';
 import 'package:capstonedesign/Game/coin_provider.dart';
 import 'first_page.dart';
